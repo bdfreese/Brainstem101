6b807a9b0be97518d0396a1c524d2b3336841fde
Cameron Ehrlich <cameronehrlich@gmail.com>
2012-11-12 03:00:55 -0800

    BEGINNING!
    This is where it all starts.

--- Brainstem101/Brainstem101/BSAppDelegate.h
+++ Brainstem101/Brainstem101/BSAppDelegate.h
@@ -4,10 +4,7 @@
 //
 //  Created by Samantha Dove on 10/3/12.
 //  Copyright (c) 2012 Brainstem101. All rights reserved.
-// berlg
-// TEST
-//BRIAN HAS MADE A CHANGE
-//BLALALALA
+//
 
 #import <UIKit/UIKit.h>
 
